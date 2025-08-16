02c6000a300a25de6158a2548a18394efb0a3a8b
JOY <joydeepnath279@gmail.com>
2025-08-16 08:33:43 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -130,7 +130,7 @@ const Header = () => {
                 onClick={clearSearch}
                 className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">
                 <img
-                  className="w-5 h-5 mix-blend-multiply"
+                  className="w-4 h-4 mix-blend-multiply"
                   src={crossIcon}
                   alt="⛌"
                 />
