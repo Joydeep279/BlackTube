da1e2d42c0ee0df239520d1048584a98ea9248a6
Joydeep <joydeepnath279@gmail.com>
2025-08-25 20:35:53 +0530

    Update Recommended.js

--- src/Components/Recommended.js
+++ src/Components/Recommended.js
@@ -5,7 +5,7 @@ const Recommended = () => {
   const [recommendedVideo, setRecommendedVideo] = useState(0);
   async function getVideoData() {
     const apiData = await fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=IN&key=AIzaSyCfUOj40aTqNvXjQsxAqDcAqBnexvcWnvw"
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=AIzaSyCfUOj40aTqNvXjQsxAqDcAqBnexvcWnvw"
     );
     const data = await apiData.json();
     setRecommendedVideo(data.items);
